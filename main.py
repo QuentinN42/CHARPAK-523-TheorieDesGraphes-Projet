fc44367836d5e1b9187953ac97f96920b0185ee7
QuentinN42 <quentin.lieumont@gmail.com>
2019-12-09 14:18:21 +0100

    function test added

--- main.py
+++ main.py
@@ -1,24 +1,56 @@
-from projet.functions import tp, ts, afficher
-from projet.tortue import graph, t
+from functions import tp, ts, afficher, CycleError
+from tortue import graph, t
 import numpy as np
 
 
-if __name__ == "__main__":
-    mat = np.array(
-        [
-            [1, 0, 0, 1, 0, 0],
-            [0, 0, 0, 1, 0, 0],
-            [0, 0, 0, 0, 1, 0],
-            [0, 0, 0, 0, 0, 0],
-            [0, 1, 0, 0, 0, 1],
-            [1, 0, 0, 0, 0, 0],
-        ]
-    )
-    _ts = ts(mat)
+def test(mat: np.ndarray, _graph: bool = False):
+    """
+    applique l'algorithme principal sur la matrice mat
+    :param mat: matrice à tester
+    :param _graph: trace un graphique turlte.py
+    """
+    assert mat.shape[0] == mat.shape[1]
+    print("="*29)
+    print("    Test sur la matrice :")
+    print("  " + " ".join(map(str, range(len(mat)))))
+    for i, l in enumerate(mat):
+        print(str(i) + " " + " ".join(map(str, l.astype(int))))
+    try:
+        _ts = ts(mat)
+    except CycleError:
+        print("Le graph est cyclique")
+        return
     _tp = tp(_ts)
     afficher(_ts, "au plus vite")
     afficher(_tp, "sequentiel")
-    graph(mat, weight=False)
-    t.up()
-    t.setpos(1000, 1000)
+    if _graph:
+        graph(mat, weight=False)
+        t.up()
+        t.setpos(1000, 1000)
+
+
+if __name__ == "__main__":
+    matrices = [
+        np.array(
+            [
+                [0, 0, 0, 1, 0, 0],
+                [0, 0, 0, 1, 0, 0],
+                [0, 0, 0, 0, 1, 0],
+                [0, 0, 0, 0, 0, 0],
+                [0, 1, 0, 0, 0, 1],
+                [1, 0, 0, 0, 0, 0],
+            ]
+        ),
+        np.array(
+            [
+                [0, 0, 1],
+                [1, 0, 1],
+                [0, 0, 0],
+            ]
+        ),
+        np.ones((5, 5)),
+        np.zeros((5, 5))
+    ]
+    for matrice in matrices:
+        test(matrice, _graph=False)
     input()
